409ce72db31863f2d15beffc55a854a79b2652f9
Michael Healy <michael@healyvectorlabs.com>
2026-04-15 05:55:03 -0400

    security: automate proprietary IP protection headers

---
+++ core/__init__.py
@@ -0,0 +1,7 @@
+# =============================================================================
+# (c) 2026 HEALY VECTOR LABS. ALL RIGHTS RESERVED.
+# This source code is the proprietary property of Michael Healy.
+# Unauthorized reproduction, distribution, or reverse-engineering is strictly
+# prohibited. This file is part of the VERITAS Auditor / FASE Engine.
+# =============================================================================
+
